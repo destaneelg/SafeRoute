a982b7548cd5c2b43b8a115d94809710d0e252e2
Aivorlan <aivorlan@icloud.com>
2020-12-19 17:57:47 -0600

    Updating picture for background and minor styling to cover page with image. Also attached search.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import background from "./img/belinda-fewings-6wAGwpsXHE0-unsplash.jpg";
+import SearchPage from './Components/SearchPage.js';
+
 
 function App() {
   return (
-    <div className="App">
+    <div container-fluid App-header className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SearchPage />
       </header>
+      
     </div>
   );
 }
