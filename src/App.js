d202734a4cf10af0ef9983dc28c956fb8f57ed23
Allyse <ayubired@gmail.com>
2020-12-19 20:11:25 -0600

    insert serch field

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import React from "react";
-import background from "./img/belinda-fewings-6wAGwpsXHE0-unsplash.jpg";
-import SearchPage from './Components/SearchPage.js';
-
+import SearchForm from "./SearchForm";
+// import background from "./img/belinda-fewings-6wAGwpsXHE0-unsplash.jpg";
+// import SearchPage from './Components/SearchPage.js';
 
 function App() {
+
+  const style = {
+    backgroundImage: "url(https://images.unsplash.com/photo-1533745848184-3db07256e163?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8d2VsY29tZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60)",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "calc(10px + 2vmin)",
+    color: "white"
+  }
+
   return (
-    <div container-fluid App-header className="App">
+    <div container-fluid App-header className="App" style = {style}>
       <header className="App-header">
-        <SearchPage />
+        {/* <SearchPage /> */}
       </header>
+      <SearchForm />
       
     </div>
   );
---
+++ src/SearchForm/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./style.css";
+
+// Using the datalist element we can create autofill suggestions based on the props.breeds array
+function SearchForm(props) {
+  return (
+    <form className="search">
+      <div className="form-group">
+        <label htmlFor="language" style={{paddingRight: "10px", fontSize: "120%"}}>Search Restrooms</label>
+        <input
+          style={{borderRadius: "15px", width: "230px", height:"30px", padding: "2px", fontStyle: "italic"}}
+          value={props.search}
+          onChange={props.handleInputChange}
+          name="term"
+          list="term"
+          type="text"
+          className="form-control"
+          placeholder="Austin,TX"
+          id="term"
+        />
+      </div>
+    </form>
+  );
+}
+
+export default SearchForm;
