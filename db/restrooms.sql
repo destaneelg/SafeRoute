2ea4efc32cb5aae03fb80ea25efbba9fe8e0c5b8
Aivorlan <aivorlan@icloud.com>
2021-01-13 14:03:45 -0600

    Restrooms db

---
+++ db/restrooms.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS restrooms_db;
+
+USE restrooms_db;
+
+CREATE TABLE IF NOT EXISTS restrooms (
+  id INT NOT NULL AUTO_INCREMENT,
+  restroom_name VARCHAR(45) NOT NULL,
+  restroom_street VARCHAR(45) NOT NULL,
+  restroom_city VARCHAR(45) NOT NULL,
+  restroom_state VARCHAR(45) NOT NULL,
+  
+  approved BOOLEAN,
+  unisex BOOLEAN,
+  PRIMARY KEY (id)
+  );
+ 
