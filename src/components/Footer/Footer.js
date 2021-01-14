a108000d7aaa53edbedfef3bf3d434e60b0987d4
Aivorlan <aivorlan@icloud.com>
2021-01-14 13:33:07 -0600

    Updated href

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "../Footer/Footer.css";
 function Footer() {
     return (
         <div className="footer">
-            <a href="#"><i class="fa fa-chevron-circle-up fa-4x"></i></a>
+            <a href="SafeRoute"><i class="fa fa-chevron-circle-up fa-4x"></i></a>
             <footer className="footer mt-auto">
                     <div className="justify-content-center">
                         <div className="social">
